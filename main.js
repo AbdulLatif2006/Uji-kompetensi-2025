f2a5f01f2ffc134205c20b54c3be2555bd3e2edd
Abdullatif2006 <sayalatif731@gmail.com>
2025-04-28 10:36:08 +0700

    menambahkan firabase

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-import { log } from './logger'
-
-log('Import works!')
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  getDoc,
+  deleteDoc,
+  doc,
+  query,
+  orderBy,
+  updateDoc
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
